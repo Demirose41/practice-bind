38c547d315772d9fd58805cee84c472fd966287f
demirose41 <demirose41@gmail.com>
2024-10-01 19:25:52 -0700

    1: Initial and final commit

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-// Your code here
+let employee = require("./employee");
+
+const john = new employee("John Wick", "Dog Lover");
+john.sayName();
+john.sayOccupation();
+setTimeout(function(){
+    john.sayName();
+}, 2000)
+setTimeout(function(){
+    john.sayOccupation();
+}, 3000)
